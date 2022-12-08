51f7355fa2d86fd55b25733cada8b2dfb62b06cf
ankitbhurji <100897459+ankitbhurji@users.noreply.github.com>
2022-12-08 13:55:27 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyright ⓒ {year}</p>
+      <p>made by Ankit Bhurji ⓒ {year}</p>
     </footer>
   );
 }
